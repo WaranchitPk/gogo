24c37ff011b295f6695f4dcd53ee5f01d1e0123b
waranchit.ch@gmail.com <Waranchit.ch@gmail.com>
2018-08-31 12:20:02 +0700

    solve show name in login drawer: member

--- src/layouts/Drawer/login.js
+++ src/layouts/Drawer/login.js
@@ -9,13 +9,8 @@ import SideDrawer from './sideList';
 
 const SideList = ({ logout, fullName }) => (
   <div>
-    {
-      fullName !== null && fullName !== undefined ? (
-        <Typography variant="display1" align="center" noWrap>{fullName.data.fullName}</Typography>
-      ) : (
-        <Typography variant="display1" align="center" noWrap>Hello</Typography>
-      )
-    }
+    <Typography variant="display1" align="center" noWrap>ผู้ใช้</Typography>
+
     <Divider/>
     <List>
       <SideDrawer logout={logout}/>
